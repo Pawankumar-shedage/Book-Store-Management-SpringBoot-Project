e267d977e34dce148621fe1edec9146a71e016bc
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-08-08 17:54:12 +0530

    first commit

---
+++ src/main/java/com/bkStore/ExcelRBookStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bkStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelRBookStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelRBookStoreProjectApplication.class, args);
+	}
+
+}
